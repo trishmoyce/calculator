cdb23e4c0436fc00509383438f1ff43557a60573
trishmoyce <itsyogyrl@gmail.com>
2022-04-12 15:37:39 -0700

    updated with code to make calc work per assignment

--- script.js
+++ script.js
@@ -5,32 +5,31 @@ function add(...numsToAdd) {
         return add1 + add2;
     });
 };
-console.log(add(10, 5));
+//console.log(add(10, 5));
 
 function subtract(...numsToSub) {
     return numsToSub.reduce((sub1, sub2) => {
         return sub1 - sub2;
     });
 };
-console.log(subtract(10, 5));
+//console.log(subtract(10, 5));
 
 function multiply(...numsToMult) {
     return numsToMult.reduce((mult1, mult2) => {
         return mult1 * mult2;
     });
 };
-console.log(multiply(10, 5));
+//console.log(multiply(10, 5));
 
 function divide(...numsToDiv) {
     return numsToDiv.reduce((div1, div2) => {
         return div1 / div2;
     });
 };
-console.log(divide(10, 5));
-
-var total;
+//console.log(divide(10, 5));
 
 function operate(operator, num1, num2) {
+var total;
     if (operator == "+") {
         return total = add(+num1, +num2);
     } else if (operator == "-") {
@@ -40,11 +39,11 @@ function operate(operator, num1, num2) {
     } else if (operator == "/") {
         return total = divide(+num1, +num2);
     } else {
-        return "error";
+        return "error!";
     }
 
 };
-console.log(operate("/", 200, 2));
+//console.log(operate("/", 200, 2));
 
 // button functions to output numbers in the display
 
@@ -123,19 +122,6 @@ function buttonDisplay(e) {
     }
 }
 
-/* if counting clicks
-
-var clickAmounts = 0;
-
-function countClicks() {
-    clickAmounts += 1;
-    console.log(clickAmounts);
-  }
-
-XX.addEventListener("click", countClicks)
-
-*/
-
 /* you can store the numbers and operators as name : value pairs in an object, but it includes a lot of manual work
 
 let calculations = {}
@@ -174,49 +160,13 @@ calculate(calculations.operator, calculations.firstNum, calculations.secondNum);
 
 */
 
-/* user enters a number, an operator, and another operator
+/*
+
+// first time a number is inputted, it is assigned to the firstNum variable, first operator is the firstOp, second number is secondNum
 
 let firstNum = [];
 let secondNum = [];
 let firstOp;
-
-function numberButton(e) {
-    if (firstOp == undefined) {
-        firstNum += e.target.value;
-    } else if (firstOp != undefined) {
-        secondNum += e.target.value;
-    }
-}
-
-function operatorButton(e) {
-    firstOp = e.target.value;
-}
-
-btn1.addEventListener("click", numberButton);
-btn2.addEventListener("click", numberButton);
-btn3.addEventListener("click", numberButton);
-btn4.addEventListener("click", numberButton);
-btn5.addEventListener("click", numberButton);
-btn6.addEventListener("click", numberButton);
-btn7.addEventListener("click", numberButton);
-btn8.addEventListener("click", numberButton);
-btn9.addEventListener("click", numberButton);
-btn0.addEventListener("click", numberButton);
-btnadd.addEventListener("click", operatorButton);
-btnsub.addEventListener("click", operatorButton);
-btnmult.addEventListener("click", operatorButton);
-btndiv.addEventListener("click", operatorButton);
-
-//the calculation is performed when entering the below function and parameters, but I need the equal button to call the function
-//operate(firstOp, firstNum, secondNum);
-
-*/
-
-let firstNum = [];
-let secondNum = [];
-let firstop;
-let secondop;
-let thirdop;
 
 btn1.addEventListener("click", calculate);
 btn2.addEventListener("click", calculate);
@@ -237,29 +187,96 @@ btnclr.addEventListener("click", pressClear);
 
 function calculate(e) {
     if (e.target.value == "+" || e.target.value == "/" || e.target.value == "-" || e.target.value == "x") {
-        firstop = e.target.value;
-        console.log(firstop);
+        firstOp = e.target.value;
+        console.log(firstOp);
     } else if (e.target.value == "1" || e.target.value == "2" || e.target.value == "3" || e.target.value == "4" || e.target.value == "5" || e.target.value == "6" || e.target.value == "7" || e.target.value == "8" || e.target.value == "9" || e.target.value == "0") {
-        if (firstop == undefined) {
+        if (firstOp == undefined) {
             firstNum += e.target.value;
             console.log(firstNum);
-        } else if (firstop != undefined) {
+        } else if (firstOp != undefined) {
             secondNum += e.target.value;
             console.log(secondNum);
         }
     }
 }
 
+// when equals is pressed, the three variables are entered as paraemters to the function which will calculate the arithmetic, and then output it in the display area
+
 function pressEquals() {
-    var equalsDoesCalc = operate(firstop, firstNum, secondNum);
+    var equalsDoesCalc = operate(firstOp, firstNum, secondNum);
     console.log(equalsDoesCalc);
     document.querySelector(".display").textContent = equalsDoesCalc;
+}
+
+// and pressing clear will reset the variables
+
+function pressClear() {
+    firstNum = "";
+    secondNum = "";
+    firstOp = undefined;
+}
+
+*/
+
+let firstNum = [];
+let secondNum = [];
+let firstOp;
+
+btn1.addEventListener("click", calculate);
+btn2.addEventListener("click", calculate);
+btn3.addEventListener("click", calculate);
+btn4.addEventListener("click", calculate);
+btn5.addEventListener("click", calculate);
+btn6.addEventListener("click", calculate);
+btn7.addEventListener("click", calculate);
+btn8.addEventListener("click", calculate);
+btn9.addEventListener("click", calculate);
+btn0.addEventListener("click", calculate);
+btnadd.addEventListener("click", calculate);
+btnsub.addEventListener("click", calculate);
+btnmult.addEventListener("click", calculate);
+btndiv.addEventListener("click", calculate);
+btnequal.addEventListener("click", pressEquals);
+btnclr.addEventListener("click", pressClear);
+
+function calculate(e) {
+    if (e.target.value == "+" || e.target.value == "/" || e.target.value == "-" || e.target.value == "x") {
+        if (firstOp == undefined) {
+            firstOp = e.target.value;
+        } else if (firstOp != undefined) {
+            var firstTotal = operate(firstOp, firstNum, secondNum);
+            firstNum = firstTotal;
+            secondNum = "";
+            firstOp = e.target.value;
+        }    
+    } else if (e.target.value == "1" || e.target.value == "2" || e.target.value == "3" || e.target.value == "4" || e.target.value == "5" || e.target.value == "6" || e.target.value == "7" || e.target.value == "8" || e.target.value == "9" || e.target.value == "0") {
+        if (firstOp == undefined) {
+            firstNum += e.target.value;
+        } else if (firstOp != undefined) {
+            secondNum += e.target.value;
+        }
+    }
+}
+
+function pressEquals() {
+    var equalsDoesCalc = operate(firstOp, firstNum, secondNum);
+    if (secondNum == '') {
+        document.querySelector(".display").textContent = "error!";
+    } else if (secondNum == 0 && firstOp == "/") {
+        document.querySelector(".display").textContent = "can't compute!";
+    } else if (equalsDoesCalc % 1 != 0) {
+        document.querySelector(".display").textContent = Math.round(equalsDoesCalc * 100) / 100;        
+        console.log(equalsDoesCalc);
+    } else {
+        document.querySelector(".display").textContent = equalsDoesCalc;
+    }
 }
 
 function pressClear() {
     firstNum = "";
     secondNum = "";
-    firstop = undefined;
+    firstOp = undefined;
 }
 
-// after a certain amount of characters entered the calculator should stop accepting input
+// other issues
+// after a certain amount of characters (no more than 11) entered the calculator should stop accepting input
