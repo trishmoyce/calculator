37da25899f6f5545a92332cd794ac17743a3cc9c
trishmoyce <itsyogyrl@gmail.com>
2022-04-15 14:30:37 -0700

    js code updated with extra credit functionality

--- script.js
+++ script.js
@@ -5,28 +5,24 @@ function add(...numsToAdd) {
         return add1 + add2;
     });
 };
-//console.log(add(10, 5));
 
 function subtract(...numsToSub) {
     return numsToSub.reduce((sub1, sub2) => {
         return sub1 - sub2;
     });
 };
-//console.log(subtract(10, 5));
 
 function multiply(...numsToMult) {
     return numsToMult.reduce((mult1, mult2) => {
         return mult1 * mult2;
     });
 };
-//console.log(multiply(10, 5));
 
 function divide(...numsToDiv) {
     return numsToDiv.reduce((div1, div2) => {
         return div1 / div2;
     });
 };
-//console.log(divide(10, 5));
 
 function operate(operator, num1, num2) {
 var total;
@@ -34,22 +30,21 @@ var total;
         return total = add(+num1, +num2);
     } else if (operator == "-") {
         return total = subtract(+num1, +num2);
-    } else if (operator == "x") {
+    } else if (operator == "x" || operator == "*") {
         return total = multiply(+num1, +num2);
     } else if (operator == "/") {
         return total = divide(+num1, +num2);
     } else {
         return "error!";
     }
-
 };
-//console.log(operate("/", 200, 2));
 
 // button functions to output numbers in the display
 
 const display = document.querySelector(".display");
 display.textContent = "";
 
+const btnback = document.querySelector(".calcbuttonback");
 const btnclr = document.querySelector(".calcbuttonclr");
 const btn1 = document.querySelector(".calcbutton1");
 const btn2 = document.querySelector(".calcbutton2");
@@ -112,7 +107,7 @@ function buttonDisplay(e) {
     } else if (buttonPressedDisplay == "-") {
         document.querySelector(".display").textContent += "-";
     } else if (buttonPressedDisplay == "x") {
-        document.querySelector(".display").textContent += "x";
+        document.querySelector(".display").textContent += "*";
     } else if (buttonPressedDisplay == "/") {
         document.querySelector(".display").textContent += "\u00f7";
     } else if (buttonPressedDisplay == ".") {
@@ -120,49 +115,9 @@ function buttonDisplay(e) {
     } else if (buttonPressedDisplay == "clear") {
         document.querySelector(".display").textContent = "";
     }
-}
+};
 
-/* you can store the numbers and operators as name : value pairs in an object, but it includes a lot of manual work
-
-let calculations = {}
-
-function numberButton(e) {
-    let storedNum = e.target.value;
-    calculations.firstNum = storedNum;
-}
-
-function operatorButton(e) {
-    let storedOperator = e.target.value;
-    calculations.operator = storedOperator;
-}
-
-function numberButton2(e) {
-    let storedNum2 = e.target.value;
-    calculations.secondNum = storedNum2;
-}
-  
-btn1.addEventListener("click", numberButton);
-btn2.addEventListener("click", numberButton);
-btn3.addEventListener("click", numberButton);
-btn4.addEventListener("click", numberButton);
-btn5.addEventListener("click", numberButton);
-btn6.addEventListener("click", numberButton2);
-btn7.addEventListener("click", numberButton2);
-btn8.addEventListener("click", numberButton2);
-btn9.addEventListener("click", numberButton2);
-btn0.addEventListener("click", numberButton2);
-btnadd.addEventListener("click", operatorButton);
-btnsub.addEventListener("click", operatorButton);
-btnmult.addEventListener("click", operatorButton);
-btndiv.addEventListener("click", operatorButton);
-
-calculate(calculations.operator, calculations.firstNum, calculations.secondNum);
-
-*/
-
-/*
-
-// first time a number is inputted, it is assigned to the firstNum variable, first operator is the firstOp, second number is secondNum
+// the calculations... first time a number is entered, it is assigned to the firstNum variable, first operator is the firstOp, second number is secondNum. If the user wants to keep calculating, these variables are all reassigned
 
 let firstNum = [];
 let secondNum = [];
@@ -182,60 +137,7 @@ btnadd.addEventListener("click", calculate);
 btnsub.addEventListener("click", calculate);
 btnmult.addEventListener("click", calculate);
 btndiv.addEventListener("click", calculate);
-btnequal.addEventListener("click", pressEquals);
-btnclr.addEventListener("click", pressClear);
-
-function calculate(e) {
-    if (e.target.value == "+" || e.target.value == "/" || e.target.value == "-" || e.target.value == "x") {
-        firstOp = e.target.value;
-        console.log(firstOp);
-    } else if (e.target.value == "1" || e.target.value == "2" || e.target.value == "3" || e.target.value == "4" || e.target.value == "5" || e.target.value == "6" || e.target.value == "7" || e.target.value == "8" || e.target.value == "9" || e.target.value == "0") {
-        if (firstOp == undefined) {
-            firstNum += e.target.value;
-            console.log(firstNum);
-        } else if (firstOp != undefined) {
-            secondNum += e.target.value;
-            console.log(secondNum);
-        }
-    }
-}
-
-// when equals is pressed, the three variables are entered as paraemters to the function which will calculate the arithmetic, and then output it in the display area
-
-function pressEquals() {
-    var equalsDoesCalc = operate(firstOp, firstNum, secondNum);
-    console.log(equalsDoesCalc);
-    document.querySelector(".display").textContent = equalsDoesCalc;
-}
-
-// and pressing clear will reset the variables
-
-function pressClear() {
-    firstNum = "";
-    secondNum = "";
-    firstOp = undefined;
-}
-
-*/
-
-let firstNum = [];
-let secondNum = [];
-let firstOp;
-
-btn1.addEventListener("click", calculate);
-btn2.addEventListener("click", calculate);
-btn3.addEventListener("click", calculate);
-btn4.addEventListener("click", calculate);
-btn5.addEventListener("click", calculate);
-btn6.addEventListener("click", calculate);
-btn7.addEventListener("click", calculate);
-btn8.addEventListener("click", calculate);
-btn9.addEventListener("click", calculate);
-btn0.addEventListener("click", calculate);
-btnadd.addEventListener("click", calculate);
-btnsub.addEventListener("click", calculate);
-btnmult.addEventListener("click", calculate);
-btndiv.addEventListener("click", calculate);
+btndec.addEventListener("click", calculate);
 btnequal.addEventListener("click", pressEquals);
 btnclr.addEventListener("click", pressClear);
 
@@ -243,20 +145,26 @@ function calculate(e) {
     if (e.target.value == "+" || e.target.value == "/" || e.target.value == "-" || e.target.value == "x") {
         if (firstOp == undefined) {
             firstOp = e.target.value;
+            document.querySelector(".calcbuttondec").disabled = false;
+//            console.log(firstOp);
         } else if (firstOp != undefined) {
             var firstTotal = operate(firstOp, firstNum, secondNum);
             firstNum = firstTotal;
             secondNum = "";
             firstOp = e.target.value;
-        }    
-    } else if (e.target.value == "1" || e.target.value == "2" || e.target.value == "3" || e.target.value == "4" || e.target.value == "5" || e.target.value == "6" || e.target.value == "7" || e.target.value == "8" || e.target.value == "9" || e.target.value == "0") {
+        }
+    } else if (e.target.value == "1" || e.target.value == "2" || e.target.value == "3" || e.target.value == "4" || e.target.value == "5" || e.target.value == "6" || e.target.value == "7" || e.target.value == "8" || e.target.value == "9" || e.target.value == "0" || e.target.value == ".") {
         if (firstOp == undefined) {
             firstNum += e.target.value;
+//            console.log(firstNum);
         } else if (firstOp != undefined) {
             secondNum += e.target.value;
+//            console.log(secondNum);
         }
     }
-}
+};
+
+// when equals is pressed, the three variables are entered as parameters to the function which will calculate the arithmetic, and then output it in the display area
 
 function pressEquals() {
     var equalsDoesCalc = operate(firstOp, firstNum, secondNum);
@@ -266,17 +174,102 @@ function pressEquals() {
         document.querySelector(".display").textContent = "can't compute!";
     } else if (equalsDoesCalc % 1 != 0) {
         document.querySelector(".display").textContent = Math.round(equalsDoesCalc * 100) / 100;        
-        console.log(equalsDoesCalc);
     } else {
         document.querySelector(".display").textContent = equalsDoesCalc;
+        console.log(equalsDoesCalc);
     }
-}
+};
+
+// coding to disable decimal button so that user doesn't add a shit ton of decimals in one number
+
+btndec.addEventListener("click", disableDecButton);
+
+function disableDecButton() {
+    document.querySelector(".calcbuttondec").disabled = true;
+};
+
+// pressing clear resets the variables
 
 function pressClear() {
     firstNum = "";
     secondNum = "";
     firstOp = undefined;
+    document.querySelector(".calcbuttondec").disabled = false;
+};
+
+// backspace functionality, applies to numbers only
+
+btnback.addEventListener("click", goBack);
+
+function goBack() {
+    if (firstNum.length =! 0 && firstOp == undefined && secondNum.length == 0) {
+        let newFirstNum = firstNum.slice(0, firstNum.length -1);
+        firstNum = newFirstNum;
+        console.log(newFirstNum);
+        document.querySelector(".display").textContent = firstNum;
+    } else if (secondNum.length =! 0) {
+        let newSecondNum = secondNum.slice(0, secondNum.length -1);
+        secondNum = newSecondNum;
+        console.log(newSecondNum);
+        document.querySelector(".display").textContent = `${firstNum}${firstOp}${secondNum}`;
+    }
+}
+
+// keyboard support for calculations
+
+document.addEventListener("keydown", logkey);
+
+function logkey(e) {
+    if (e.key == "Escape") {
+        pressClear();
+    } else if (e.key == "+" || e.key == "/" || e.key == "-" || e.key == "*") {
+        if (firstOp == undefined) {
+            firstOp = e.key;
+            document.querySelector(".calcbuttondec").disabled = false;
+            console.log(firstOp);
+        } else if (firstOp != undefined) {
+            var firstTotal = operate(firstOp, firstNum, secondNum);
+            firstNum = firstTotal;
+            secondNum = "";
+            firstOp = e.key;
+        }
+    } else if (e.key == 0 || e.key == 1 || e.key == 2 || e.key == 3 || e.key == 4 || e.key == 5 || e.key == 6 || e.key == 7 || e.key == 8 || e.key == 9 || e.key == ".") {
+        if (firstOp == undefined) {
+            firstNum += e.key;
+            console.log(firstNum);
+        } else if (firstOp != undefined) {
+            secondNum += e.key;
+            console.log(secondNum);
+        }
+    } else if (e.key == "Enter" || e.key == "=") {
+        var enterDoesCalc = operate(firstOp, firstNum, secondNum);
+        if (secondNum == '') {
+            document.querySelector(".display").textContent = "error!";
+        } else if (secondNum == 0 && firstOp == "/") {
+            document.querySelector(".display").textContent = "can't compute!";
+        } else if (enterDoesCalc % 1 != 0) {
+            document.querySelector(".display").textContent = Math.round(enterDoesCalc * 100) / 100;        
+        } else {
+            document.querySelector(".display").textContent = enterDoesCalc;
+            console.log(enterDoesCalc);
+        }
+    } else if (e.key == "Backspace") {
+        goBack();
+    }
+}
+
+// keyboard display coding
+
+document.addEventListener("keydown", displaykey);
+
+function displaykey(e) {
+    if (e.key == 0 || e.key == 1 || e.key == 2 || e.key == 3 || e.key == 4 || e.key == 5 || e.key == 6 || e.key == 7 || e.key == 8 || e.key == 9 || e.key == "+" || e.key == "-" || e.key == "/" || e.key == "*" || e.key == ".")  {
+        document.querySelector(".display").textContent += `${e.key}`;
+    } else if (e.key == "Escape") {
+        document.querySelector(".display").textContent = "";
+    }
 }
 
 // other issues
-// after a certain amount of characters (no more than 11) entered the calculator should stop accepting input
+// make it pretty
+// after a certain amount of characters (no more than 11) entered the calculator should stop accepting input (if str.length > 11)
